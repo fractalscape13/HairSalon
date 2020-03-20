1c24ae824fa9f6ac5e7b24e52c5385e83e5bc11a
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-20 08:47:00 -0700

    add homecontroller.cs

---
+++ HairSalon/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HairSalon.Models;
+
+
+namespace HairSalon.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
