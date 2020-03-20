46799f6d65c92fe4c1fb33b050585c77de7a35b4
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-20 08:44:46 -0700

    create stylist.cs model

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Hours { get; set; }
+    public string KeyWords { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
+
+}
